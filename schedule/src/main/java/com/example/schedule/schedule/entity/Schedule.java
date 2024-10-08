69fc09e7bf02ae0751f915ef89763cae901f7968
haeun29 <hanh291029@naver.com>
2024-10-08 12:21:18 +0900

    time in future

--- schedule/src/main/java/com/example/schedule/schedule/entity/Schedule.java
+++ schedule/src/main/java/com/example/schedule/schedule/entity/Schedule.java
@@ -3,6 +3,8 @@ package com.example.schedule.schedule.entity;
 import com.example.schedule.entity.BaseEntity;
 import com.example.schedule.user.entity.UserEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -25,8 +27,10 @@ public class Schedule extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Location destination;
     @Setter
+    @FutureOrPresent(message = "Start time must be present or future")
     private LocalDateTime startTime;
     @Setter
+    @Future(message = "End time must be in the future")
     private LocalDateTime endTime;
     @Enumerated(EnumType.STRING)
     private TransOption.TransMode mode;
