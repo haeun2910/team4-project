a00593c5cfac708a8fc2a5080af8a981b67d93ac
haeun29 <hanh291029@naver.com>
2024-10-10 14:10:16 +0900

    chang class name

--- schedule/src/test/java/com/example/schedule/ScheduleApplicationTests.java
+++ schedule/src/test/java/com/example/schedule/ScheduleApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PlanApplicationTests {
+class ScheduleApplicationTests {
 
 	@Test
 	void contextLoads() {
