8a3f7cd4f019ec1cd156c46622430ae6d331e4af
haeun29 <hanh291029@naver.com>
2024-10-24 13:39:02 +0900

    naver map marker

--- schedule/src/main/java/com/example/moveSmart/TestController.java
+++ schedule/src/main/java/com/example/moveSmart/TestController.java
@@ -3,20 +3,27 @@ package com.example.moveSmart;
 import com.example.moveSmart.auth.jwt.JwtTokenUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("test")
 public class TestController {
     private final JwtTokenUtils jwtTokenUtils;
     public TestController(JwtTokenUtils jwtTokenUtils) {
         this.jwtTokenUtils = jwtTokenUtils;
     }
-    @GetMapping("test")
+    @GetMapping
     public String test() {
         return "test";
     }
     @GetMapping("search")
     public String test2() {
-        return "user/api/search";
+        return "navermap";
+    }
+    @GetMapping("plan-create")
+    public String test3() {
+        return "createPlan";
     }
 
 
