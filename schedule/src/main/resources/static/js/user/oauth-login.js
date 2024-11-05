654617aba4b0a06ff5778636e99e0e6fd4b01f46
Dung Thùy <dungkiu476@gmail.com>
2024-11-05 13:37:29 +0900

    update oauth2

--- schedule/src/main/resources/static/js/user/oauth-login.js
+++ schedule/src/main/resources/static/js/user/oauth-login.js
@@ -5,7 +5,7 @@ const token = urlParams.get('token');
 
 if (token) {
     // Lưu token vào localStorage
-    localStorage.setItem('jwt', token);
+    localStorage.setItem('token', token);
 
     // Chuyển hướng đến trang chính hoặc dashboard
     window.location.href = "/views";  // Thay đổi thành trang bạn muốn chuyển hướng đến
